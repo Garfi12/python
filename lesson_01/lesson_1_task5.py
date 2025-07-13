2f40bde7a476fd2d4fb85dae463ecadce808c35b
Max Kuz <m001_01@mail.ru>
2025-07-13 23:22:01 +0500

    t com

---
+++ lesson_01/lesson_1_task5.py
@@ -0,0 +1,15 @@
+def print_number(mum):
+    print(mum, end='')
+
+
+print_number('8')
+print_number('8')
+print_number('0')
+print_number('0')
+print_number('5')
+print_number('5')
+print_number('5')
+print_number('3')
+print_number('5')
+print_number('3')
+print_number('5')
