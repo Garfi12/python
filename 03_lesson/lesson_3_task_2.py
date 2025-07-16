8b069bc7b4973ecb14a7162242a7f3387e3c0c70
Max Kuz <m001_01@mail.ru>
2025-07-17 00:31:29 +0500

    3 com

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,14 +1,14 @@
 from smartphone import Smartphone
 
-phone1 = Smartphone
-phone2 = Smartphone
-phone3 = Smartphone
-phone4 = Smartphone
-phone1 = ("Apple", "12 mini", "+79990076545")
-phone2 = ("Lenovo", "12", "+79990062212")
-phone3 = ("Honor", "30i", "+79050062212")
-phone4 = ("Samsung", "M31S", "+79990555552")
+    phone1 = Smartphone
+    phone2 = Smartphone
+    phone3 = Smartphone
+    phone4 = Smartphone
+    phone1 = ("Apple", "12 mini", "+79990076545")
+    phone2 = ("Lenovo", "12", "+79990062212")
+    phone3 = ("Honor", "30i", "+79050062212")
+    phone4 = ("Samsung", "M31S", "+79990555552")
 
-catalog = [phone1, phone2, phone3, phone4]
+    catalog = [phone1, phone2, phone3, phone4]
 
-print(catalog)
+    print(catalog)
