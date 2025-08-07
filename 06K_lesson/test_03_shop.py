5cfba28f5c5fe0a6444712ded7022f753880ec64
Max Kuz <m001_01@mail.ru>
2025-08-07 15:41:16 +0500

    6K com

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,50 +1,32 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages import LoginPage, InventoryPage, CartPage, CheckoutPage, CheckoutOverviewPage
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
+
+def test_shopping_flow(driver):
+
+    driver.get("https://www.saucedemo.com/")
 
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-waiter = WebDriverWait(driver,5)
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
 
-driver.get("https://www.saucedemo.com/")
-login = driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
-password = driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
-driver.find_element(By.CSS_SELECTOR, "#login-button").click()
-waiter.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div[class="app_logo"]'), "Swag Labs")
-)
+    inventory_page = InventoryPage(driver)
+    inventory_page.add_item_to_cart("Sauce Labs Backpack")
+    inventory_page.add_item_to_cart("Sauce Labs Bolt T-Shirt")
+    inventory_page.add_item_to_cart("Sauce Labs Onesie")
 
-driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
-driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
+    inventory_page.go_to_cart()
+    cart_page = CartPage(driver)
+    cart_page.proceed_to_checkout()
+    checkout_page = CheckoutPage(driver)
+    checkout_page.fill_info("Иван", "Петров", "123456")
 
-driver.find_element(By.CSS_SELECTOR, "#shopping_cart_container").click()
-driver.find_element(By.CSS_SELECTOR, "#checkout").click()
-waiter.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div[class="app_logo"]'), "Swag Labs")
-)
-first_n = driver.find_element(By.CSS_SELECTOR, "#first-name").send_keys("Maksim")
-last_n = driver.find_element(By.CSS_SELECTOR, "#last-name").send_keys("Kuzmenkin")
-zipcode = driver.find_element(By.CSS_SELECTOR, "#postal-code").send_keys("640002")
-driver.find_element(By.CSS_SELECTOR, "#continue").click()
-waiter.until(
-    EC.text_to_be_present_in_element((By.XPATH, '//*[@id="header_container"]/div[2]/span'), "Checkout: Overview")
-)
-
-
-@pytest.mark.test_shop
-@pytest.mark.parametrize('res_in, res_es', [
-        (driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[8]').get_attribute("textContent"), 'Total: $58.29')
-        ])
-def test_price (res_in, res_es):
-    assert res_in == res_es
-    if res_in == res_es:
-        print("success")
-    else:
-        print("false")  
-
-driver.quit()
+    overview_page = CheckoutOverviewPage(driver)
+    total = overview_page.get_total()
+    assert total == "$58.29", f"Ожидалась сумма $58.29, получено {total}"
